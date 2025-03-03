79c098b6d195ff103ac50a548b202ce05f39d5da
Kyle Larkin <kyle.larkin@U-2U1H90H2EWXD5.nextgenwaterprediction.com>
2025-03-03 10:37:25 -0500

    re-packaged map and ts together. now parses ts dates from ngen csv files. updated sample wrapper scripts. changed main() functions to more descriptive names.

--- swe_timeseries/wrapper.py
+++
@@ -1,9 +0,0 @@
-from swe_timeseries.swe_timeseries import main
-
-main(['sample_csv/09359500', 
-	  'sample_gpkg/gages-09359500.gpkg',
-	  '2019-10-02',
-	  '2020-09-29',
-	  '--plot_output', 'comb_plot.png',
-	  '--csv_output','comb_table.csv'
-	  ])
