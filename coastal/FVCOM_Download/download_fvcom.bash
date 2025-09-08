49085ccbaccad5c955626dd49b58f859717af145
zhengtaocui <Zhengtao.Cui@rtx.com>
2025-09-08 15:02:25 +0000

    1) Updated the FVCOM_download script to be compatiable with the recent updates (removed the regulargrid and forecast files) on the Amazon AWS server. 2) Added the forcing download script.

--- coastal/FVCOM_Download/download_fvcom.bash
+++ coastal/FVCOM_Download/download_fvcom.bash
@@ -6,17 +6,22 @@ else
     ROOT_SHARE_TXT="ROOT_SHARE set to: $ROOT_SHARE"
 fi
 
-usage() { echo -e "Usage: $0 [-d <utcdate>(yyyymmdd)] [-n <domain> (one of leofs, lmhofs, loofs, or lsofs)] [-o <output path>]\n\tdefaults: \n\t\t<utcdate>: current utc day\n\t\t<domain>: all 4 domains\n\t\t<output path>: \$ROOT_SHARE/data)" 1>&2; exit 1; }
+usage() { echo -e "Usage: $0 [-s <start_utcdate>(yyyymmdd)]  [-e <end_utcdate>(yyyymmdd)] [-n <domain> (one of leofs, lmhofs, loofs, or lsofs)] [-o <output path>]\n\tdefaults: \n\t\t<start_utcdate>: current utc day\n\t\t<end_utcdate>: current utc day + 1 day\n\t\t<domain>: all 4 domains\n\t\t<output path>: \$ROOT_SHARE/data)" 1>&2; exit 1; }
 
-options=$(getopt -o d:n:o:h --long utcdate:,domain:,output:,help -- "$@")
+options=$(getopt -o s:e:n:o:h --long start_utcdate:,end_utcdate:,domain:,output:,help -- "$@")
 eval set -- "$options"
 
 while :; do
     case "$1" in
-        -d|--utcdate)
+        -s|--start_utcdate)
             shift
-            INPUT_DATE=$1
-            echo "got option: $INPUT_DATE"
+            INPUT_DATE_START=$1
+            echo "got option: $INPUT_DATE_START"
+            ;;
+        -e|--end_utcdate)
+            shift
+            INPUT_DATE_END=$1
+            echo "got option: $INPUT_DATE_END"
             ;;
         -n|--domain)
             shift
@@ -41,11 +46,18 @@ while :; do
     shift
 done
 
-if [[ -z $INPUT_DATE ]]; then
-    UTC_DATE=`date "+%Y%m%d"`
+if [[ -z $INPUT_DATE_START ]]; then
+    UTC_DATE_START=`date "+%Y%m%d"`
 else
-    echo "Date specified on cmd line... `date -d $INPUT_DATE "+%Y%m%d"`"
-    UTC_DATE=`date -d $INPUT_DATE "+%Y%m%d"`
+    echo "Date specified on cmd line... `date -d $INPUT_DATE_START "+%Y%m%d"`"
+    UTC_DATE_START=`date -d $INPUT_DATE_START "+%Y%m%d"`
+fi
+
+if [[ -z $INPUT_DATE_END ]]; then
+    UTC_DATE_END=`date '+%C%y%m%d' -d "+1 days""`
+else
+    echo "Date specified on cmd line... `date -d $INPUT_DATE_END "+%Y%m%d"`"
+    UTC_DATE_END=`date -d $INPUT_DATE_END "+%Y%m%d"`
 fi
 
 if [[ -z $OPTION_DOMAIN ]]; then
@@ -65,7 +77,7 @@ else
     OUTPUT_DIR="$OPTION_DIR"
 fi
 
-pdy=$UTC_DATE
+pdy=$UTC_DATE_START
 recent_two_month=$(date -d "1 month ago" "+%Y%m")01
 
 #=========================================================
@@ -112,6 +124,12 @@ recent_two_month=$(date -d "1 month ago" "+%Y%m")01
 #   N/A
 #=========================================================
 
+declare -A domain_to_lake=( \
+	   [leofs]="erie" \
+	   [lmhofs]="michigan-and-huron" \
+	   [loofs]="ontario" \
+	   [lsofs]="superior" )
+
 for domain in "${DOMAINS[@]}"; do
      
    OFSDIR=$OUTPUT_DIR/${domain}
@@ -121,54 +139,64 @@ for domain in "${DOMAINS[@]}"; do
          
    cd $OFSDIR
 
+   #URLBASE=https://www.ncei.noaa.gov/data/operational-nowcast-and-forecast-hydrodynamic-model-systems-co-ops/access/lake-${domain_to_lake[$domain]}-operational-forecast-system-${domain}/
+   URLBASE=https://noaa-nos-ofs-pds.s3.amazonaws.com/${domain}/netcdf
+  while [ $pdy -lt $UTC_DATE_END ]; do
    for cyc in 00 06 12 18; do
      #pre 202301
      if [ ${pdy} -lt 20230101 ]; then
         for i in {000..006}; do 
             wget -nc --no-check-certificate \
-	        https://noaa-nos-ofs-pds.s3.amazonaws.com/${domain}/netcdf/${pdy:0:-2}/nos.${domain}.fields.n${i}.${pdy}.t${cyc}z.nc
+	        ${URLBASE}/${pdy:0:-2}/nos.${domain}.fields.n${i}.${pdy}.t${cyc}z.nc
+	        #${URLBASE}/${pdy:0:4}/${pdy:4:2}/nos.${domain}.fields.n${i}.${pdy}.t${cyc}z.nc
         done
      
      #pre 202403
      elif [ ${pdy} -lt 20240301 ]; then
         for i in {000..006}; do 
             wget -nc --no-check-certificate \
-	        https://noaa-nos-ofs-pds.s3.amazonaws.com/${domain}/netcdf/${pdy:0:-2}/nos.${domain}.fields.n${i}.${pdy}.t${cyc}z.nc
+	        ${URLBASE}/${pdy:0:-2}/nos.${domain}.fields.n${i}.${pdy}.t${cyc}z.nc
+	        #${URLBASE}/${pdy:0:4}/${pdy:4:2}/nos.${domain}.fields.n${i}.${pdy}.t${cyc}z.nc
         done
         wget -nc --no-check-certificate \
-	        https://noaa-nos-ofs-pds.s3.amazonaws.com/${domain}/netcdf/${pdy:0:-2}/nos.${domain}.stations.nowcast.${pdy}.t${cyc}z.nc
+	        ${URLBASE}/${pdy:0:-2}/nos.${domain}.stations.nowcast.${pdy}.t${cyc}z.nc
+	        #${URLBASE}/${pdy:0:4}/${pdy:4:2}/nos.${domain}.stations.nowcast.${pdy}.t${cyc}z.nc
      else
-        if [ ${pdy} -lt $recent_two_month ]; then
-           URL=https://noaa-nos-ofs-pds.s3.amazonaws.com/${domain}/netcdf/${pdy:0:-2}
-        else
-           URL=https://noaa-nos-ofs-pds.s3.amazonaws.com/${domain}/netcdf/${pdy:0:4}/${pdy:4:2}/${pdy:6:2}
-        fi
+#        if [ ${pdy} -lt $recent_two_month ]; then
+#           #URL=https://noaa-nos-ofs-pds.s3.amazonaws.com/${domain}/netcdf/${pdy:0:-2}
+#	   URL=https://www.ncei.noaa.gov/data/operational-nowcast-and-forecast-hydrodynamic-model-systems-co-ops/access/lake-${domain_to_lake[$domain]}-operational-forecast-system-${domain}/${pdy:0:4}/${pdy:4:2}
+#        else
+#           URL=https://www.ncei.noaa.gov/data/operational-nowcast-and-forecast-hydrodynamic-model-systems-co-ops/access/lake-${domain_to_lake[$domain]}-operational-forecast-system-${domain}/${pdy:0:4}/${pdy:4:2}
+#        fi
 
+        #URL=https://www.ncei.noaa.gov/data/operational-nowcast-and-forecast-hydrodynamic-model-systems-co-ops/access/lake-${domain_to_lake[$domain]}-operational-forecast-system-${domain}/${pdy:0:4}/${pdy:4:2}
+        URL=https://noaa-nos-ofs-pds.s3.amazonaws.com/${domain}/netcdf/${pdy:0:-2}
 	#nowcast station
         wget -nc --no-check-certificate \
 	        ${URL}/${domain}.t${cyc}z.${pdy}.stations.nowcast.nc
 	#nowcast regulargrid and grid
-        for i in {000..006}; do 
-            wget -nc --no-check-certificate \
-	        ${URL}/${domain}.t${cyc}z.${pdy}.regulargrid.n${i}.nc
-            wget -nc --no-check-certificate \
-	        ${URL}/${domain}.t${cyc}z.${pdy}.fields.n${i}.nc
-        done
+#        for i in {000..006}; do 
+#            wget -nc --no-check-certificate \
+#	        ${URL}/${domain}.t${cyc}z.${pdy}.regulargrid.n${i}.nc
+#            wget -nc --no-check-certificate \
+#	        ${URL}/${domain}.t${cyc}z.${pdy}.fields.n${i}.nc
+#        done
 
 	#forecast station
         wget -nc --no-check-certificate \
 		${URL}/${domain}.t${cyc}z.${pdy}.stations.forecast.nc
 	#forecast regulargrid and grid
-        for i in {000..120}; do 
-            wget -nc --no-check-certificate \
-	        ${URL}/${domain}.t${cyc}z.${pdy}.regulargrid.f${i}.nc
-            wget -nc --no-check-certificate \
-	        ${URL}/${domain}.t${cyc}z.${pdy}.fields.f${i}.nc
-        done
+#        for i in {000..120}; do 
+#            wget -nc --no-check-certificate \
+#	        ${URL}/${domain}.t${cyc}z.${pdy}.regulargrid.f${i}.nc
+#            wget -nc --no-check-certificate \
+#	        ${URL}/${domain}.t${cyc}z.${pdy}.fields.f${i}.nc
+#        done
      fi
    done
-
-   cd -
+   pdy=$(date '+%C%y%m%d' -d "$pdy+1 days") 
+  done
+  cd -
 
 done
 
