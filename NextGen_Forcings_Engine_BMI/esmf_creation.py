55502139468374132edb6d20b2ca99085e85f632
jeff.wade <jeff.s.wade@rtx.com>
2025-09-16 10:26:33 -0700

    Move esmf_creation to a separate script

---
+++ NextGen_Forcings_Engine_BMI/esmf_creation.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+from ESMF_Mesh_Domain_Configuration_Production.NextGen_hyfab_to_ESMF_Mesh import convert_hyfab_to_esmf
+# import /NextGen_hyfab_to_ESMF_Mesh.py
+
+def create_mesh(cfg: dict, hyfab_name: str):
+    """
+    Create ESMF Mesh from geopackage file provided by the forcing engine config
+
+    :param cfg: dictionary of forcing engine config parameters
+    :param hyfab_name: path to hydrofabric geopackage file (we might move this to the config file)
+    """
+
+    # Set the mesh file name based on the hydrofabric file
+    base_geo_name = os.path.splitext(os.path.basename(hyfab_name))[0]
+    mesh_fileName = f"{base_geo_name}_ESMF_Mesh.nc"
+
+    # Extract set global paths
+    mesh_outPath = os.path.join("/ngen-app/data/esmf_mesh/", mesh_fileName)
+
+    # Check if the mesh file already exists and skip conversion if it does
+    if not os.path.exists(mesh_outPath):
+        convert_hyfab_to_esmf(
+            hyfab_gpkg=hyfab_name,
+            esmf_mesh_output=mesh_outPath
+        )
+    else:
+        print(f"ESMF mesh file already exists at {mesh_outPath}, skipping conversion.")
