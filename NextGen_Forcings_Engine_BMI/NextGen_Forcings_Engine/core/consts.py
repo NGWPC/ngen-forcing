18933758698d165d3a182e8f31c6298ccac25ad6
Matthew.Deshotel <mdeshotel@dewberry.com>
2026-02-13 17:05:57 -0600

    add consts.py

---
+++ NextGen_Forcings_Engine_BMI/NextGen_Forcings_Engine/core/consts.py
@@ -0,0 +1,11 @@
+from NextGen_Forcings_Engine_BMI.NextGen_Forcings_Engine.core.geoMod import (
+    GriddedGeoMeta,
+    HydrofabricGeoMeta,
+    UnstructuredGeoMeta,
+)
+
+GEOGRID = {
+    "gridded": GriddedGeoMeta,
+    "unstructured": UnstructuredGeoMeta,
+    "hydrofabric": HydrofabricGeoMeta,
+}
