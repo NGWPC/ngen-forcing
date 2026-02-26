f77849f6a77289bbac1ced5d3d46b5991498c0c2
Max Kipp <jkipp@dewberry.com>
2026-02-25 21:57:43 -0500

    Improve comments

--- NextGen_Forcings_Engine_BMI/NextGen_Forcings_Engine/general_utils.py
+++ NextGen_Forcings_Engine_BMI/NextGen_Forcings_Engine/general_utils.py
@@ -4,13 +4,19 @@ import functools
 
 
 def setter_hardener(func):
-    """Decorator for setters. Causes the setter to be hardened such that it
+    """
+    Decorator for setters. Causes the setter to be hardened such that it
     asserts that the new value is either replacing None, or that the new value
-    is equal to the existing value."""
+    is equal to the existing value.
+
+    NOTE: since `==` is used to determine if the new value is equal to the old value,
+    it is possible to change the value if the `==` check passes, for example setting 5.0 (float) to replace 5 (int)
+    would not cause an error to be raised.
+    """
 
     @functools.wraps(func)
     def wrapper(self, new_value):
-        # Private attr attr_public is typical pattern, with underscore prepending the public attr attr_public.
+        # Private attr attr_private is typical pattern, with underscore prepending the public attr attr_public.
         attr_public = func.__name__
         attr_private = f"_{attr_public}"
         # Current value, or None if not yet set.
