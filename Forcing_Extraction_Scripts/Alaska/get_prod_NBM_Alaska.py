61893f9e56646169bdf8bc144a45ad33fcd31cdd
jeff.wade <jeff.s.wade@rtx.com>
2026-08-17 12:24:46 -0400

    Update InputOffsets and NBM AK handling

--- Forcing_Extraction_Scripts/Alaska/get_prod_NBM_Alaska.py
+++ Forcing_Extraction_Scripts/Alaska/get_prod_NBM_Alaska.py
@@ -16,11 +16,30 @@ class NBMAlaskaDownloader(ForecastDownloader):
         return "https://noaa-nbm-grib2-pds.s3.amazonaws.com"
 
     def get_download_targets(self, d_start):
-        hourly = range(1, 37)  # 1 through 36
-        every_3h = range(36, 193, 3)  # 123 through 240, step of 3
-        every_6h = range(198, 265, 6)  # 198 through 264, step of 6
-        return list(hourly) + list(every_3h) + list(every_6h) if d_start.hour in [0, 6, 12, 18] else range(1,19)
-        # return range(1, 265) if d_start.hour in [0, 6, 12, 18] else []
+        if self.input_horizon == 15:
+            # Short range Alaska: cycles at 00/06/12/18Z, hourly f001-f018
+            if d_start.hour not in (0, 6, 12, 18):
+                return []
+            return list(range(1, 19))
+
+        elif self.input_horizon == 45:
+            # Short range extended Alaska: cycles at 03/09/15/21Z, hourly f001-f048
+            if d_start.hour not in (3, 9, 15, 21):
+                return []
+            return list(range(1, 49))
+
+        elif self.input_horizon == 240:
+            # Medium Range Alaska: cycles at 00/06/12/18Z, tiered hourly/3h/6h to f264
+            if d_start.hour not in (0, 6, 12, 18):
+                return []
+            hourly = range(1, 37)  # 1 through 36
+            every_3h = range(36, 193, 3)  # 123 through 240, step of 3
+            every_6h = range(198, 265, 6)  # 198 through 264, step of 6
+            return list(hourly) + list(every_3h) + list(every_6h)
+
+        else:
+            # Unexpected AK forecast length
+            return []
 
     def build_output_dir(self, d_start, _):
         return os.path.join(
