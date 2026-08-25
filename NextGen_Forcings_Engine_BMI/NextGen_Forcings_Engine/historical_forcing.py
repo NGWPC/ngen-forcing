d94cd2d7b92b0d1c37cb29b945ba9cfd0ad1a1dd
Max Kipp <jkipp@dewberry.com>
2026-08-25 18:14:51 -0400

    Remove unused variable

--- NextGen_Forcings_Engine_BMI/NextGen_Forcings_Engine/historical_forcing.py
+++ NextGen_Forcings_Engine_BMI/NextGen_Forcings_Engine/historical_forcing.py
@@ -657,10 +657,6 @@ class NWMV3ConusProcessor(NWMV3Processor):
         for var in self.vars:
             try:
                 with self.timing_block(f"lazy loading {self.dataset_name} data"):
-                    # TODO this object_store var is not used
-                    object_store = obstore.store.from_url(
-                        self.url(var), skip_signature=True
-                    )
                     datasets.append(self.slice_ds(self.s3_lazy_ds[var]))
             except Exception as e:
                 LOG.critical(
